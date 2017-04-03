d783885f4317183a10b46491ba4dfcf3769bca3c
Marco <marco1@live.de>
2017-04-03 09:57:47 +0200

    Add 'DATA_TYPE_TEXT' constant and implementation to class 'Input'

--- src/Input.php
+++ src/Input.php
@@ -11,7 +11,7 @@ namespace Delight\Foundation;
 /** Input validation and filtering */
 final class Input {
 
-	/** Validates a value as a string by removing control characters and whitespace */
+	/** Validates a value as a single-line string by removing control characters and leading or trailing whitespace */
 	const DATA_TYPE_STRING = 1;
 	/** Validates a value as an integer number and returns `null` for all invalid values */
 	const DATA_TYPE_INT = 2;
@@ -25,6 +25,8 @@ final class Input {
 	const DATA_TYPE_URL = 6;
 	/** Validates a value as an IP address (IPv4 or IPv6) and returns `null` for all invalid values */
 	const DATA_TYPE_IP = 7;
+	/** Validates a value as a multi-line string by removing control characters (except for tabs and newlines) and leading or trailing whitespace */
+	const DATA_TYPE_TEXT = 8;
 
 	/**
 	 * Returns a validated, filtered and typecast value from the `GET` variables
@@ -77,7 +79,7 @@ final class Input {
 			$value = \trim($value);
 		}
 
-		$filtered = \filter_var($value, self::determineFilterMode($type), self::composeFilterFlags());
+		$filtered = \filter_var($value, self::determineFilterMode($type), self::composeFilterFlags($type));
 
 		return self::applyPostProcessing($filtered, $type);
 	}
@@ -98,7 +100,7 @@ final class Input {
 
 		$type = self::dataTypeOrDefault($type);
 
-		$filtered = \filter_input($source, $key, self::determineFilterMode($type), self::composeFilterFlags());
+		$filtered = \filter_input($source, $key, self::determineFilterMode($type), self::composeFilterFlags($type));
 
 		return self::applyPostProcessing($filtered, $type);
 	}
@@ -141,15 +143,20 @@ final class Input {
 	/**
 	 * Builds a bitmask representing filter options as understood by {@see \filter_input} and {@see \filter_var}
 	 *
+	 * @param int $type one of the constants from this class starting with `DATA_TYPE_`
 	 * @return int the bitmask consisting of individual flags
 	 */
-	private static function composeFilterFlags() {
+	private static function composeFilterFlags($type) {
 		$flags = 0;
 
 		$flags |= \FILTER_NULL_ON_FAILURE;
 		$flags |= \FILTER_FLAG_STRIP_BACKTICK;
 		$flags |= \FILTER_FLAG_NO_ENCODE_QUOTES;
-		$flags |= \FILTER_FLAG_STRIP_LOW;
+
+		if ($type !== self::DATA_TYPE_TEXT) {
+			$flags |= \FILTER_FLAG_STRIP_LOW;
+		}
+
 		$flags |= \FILTER_FLAG_ALLOW_FRACTION;
 		$flags |= \FILTER_FLAG_IPV4;
 		$flags |= \FILTER_FLAG_IPV6;
@@ -165,8 +172,13 @@ final class Input {
 	 * @return mixed|null the processed value
 	 */
 	private static function applyPostProcessing($value, $type) {
-		if ($type === self::DATA_TYPE_STRING) {
+		if ($type === self::DATA_TYPE_STRING || $type === self::DATA_TYPE_TEXT) {
 			$value = \trim($value);
+		}
+
+		if ($type === self::DATA_TYPE_TEXT) {
+			// remove control characters (ASCII characters 0 to 31) except for tabs and newlines
+			$value = \preg_replace('/[\x00-\x08\x0b-\x0c\x0e-\x1f]/', '', $value);
 		}
 
 		return $value;
