9c8011c1a1252f4f964b1e353023674764d3e4d6
Marco <marco@delight.im>
2019-02-09 00:43:59 +0100

    Add base classes for all internal exceptions and errors

---
+++ src/Throwable/Error.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+ * PHP-Foundation-Core (https://github.com/delight-im/PHP-Foundation-Core)
+ * Copyright (c) delight.im (https://www.delight.im/)
+ * Licensed under the MIT License (https://opensource.org/licenses/MIT)
+ */
+
+namespace Delight\Foundation\Throwable;
+
+/** Base class for all (unchecked) errors */
+abstract class Error extends \Exception {}
---
+++ src/Throwable/Exception.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+ * PHP-Foundation-Core (https://github.com/delight-im/PHP-Foundation-Core)
+ * Copyright (c) delight.im (https://www.delight.im/)
+ * Licensed under the MIT License (https://opensource.org/licenses/MIT)
+ */
+
+namespace Delight\Foundation\Throwable;
+
+/** Base class for all (checked) exceptions */
+abstract class Exception extends \Exception {}
