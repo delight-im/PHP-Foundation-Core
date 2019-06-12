d509c8bea0d0d65a1ed032e549d0de0d7d092fc0
Marco <marco@delight.im>
2019-06-12 20:35:36 +0200

    Create 'NoSupportedLocaleError'

---
+++ src/Throwable/NoSupportedLocaleError.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+ * PHP-Foundation-Core (https://github.com/delight-im/PHP-Foundation-Core)
+ * Copyright (c) delight.im (https://www.delight.im/)
+ * Licensed under the MIT License (https://opensource.org/licenses/MIT)
+ */
+
+namespace Delight\Foundation\Throwable;
+
+/** Error that is thrown when accessing the internationalization component without supporting any locale */
+class NoSupportedLocaleError extends Error {}
